ece556d202613fe32178f16f057c1988fc9f50a3
joucarvalho <joucarvalho18@icloud.com>
2026-06-02 21:55:38 -0300

    fix: self-host fontes via @fontsource para corrigir PDF em produção

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,14 @@
+import "@fontsource/dm-sans/400.css";
+import "@fontsource/dm-sans/500.css";
+import "@fontsource/dm-sans/600.css";
+import "@fontsource/dm-sans/700.css";
+import "@fontsource/ibm-plex-mono/400.css";
+import "@fontsource/ibm-plex-mono/500.css";
+import "@fontsource/ibm-plex-mono/600.css";
+import "@fontsource/source-sans-3/300.css";
+import "@fontsource/source-sans-3/400.css";
+import "@fontsource/source-sans-3/500.css";
+import "@fontsource/source-sans-3/600.css";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
